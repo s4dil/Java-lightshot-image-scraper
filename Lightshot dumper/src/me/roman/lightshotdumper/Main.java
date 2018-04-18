1a5f214cd2a396552781adfeda4ab7dcb29b75c0
Roman <Roman@DESKTOP-8LDQC8B>
2018-04-18 23:41:50 +0200

    Update

--- Lightshot dumper/src/me/roman/lightshotdumper/Main.java
+++ Lightshot dumper/src/me/roman/lightshotdumper/Main.java
@@ -7,6 +7,10 @@ import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 public class Main {
+	
+	/**
+	 * MADE BY ROMAN >>     https://github.com/KaktusWasser
+	 */
 
 	public static void main(String[] args) throws ClassNotFoundException, InstantiationException,
 			IllegalAccessException, UnsupportedLookAndFeelException, IOException {
