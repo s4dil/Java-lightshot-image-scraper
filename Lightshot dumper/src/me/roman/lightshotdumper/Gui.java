55277de5dcbf409e624f907195f5bc99c4b51626
Roman <Roman@DESKTOP-8LDQC8B>
2018-04-18 23:38:29 +0200

    Second update.

--- Lightshot dumper/src/me/roman/lightshotdumper/Gui.java
+++ Lightshot dumper/src/me/roman/lightshotdumper/Gui.java
@@ -3,34 +3,39 @@ package me.roman.lightshotdumper;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.border.EmptyBorder;
 
 import me.roman.lightshotdumper.utils.Utils;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.JSpinner;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import java.awt.Color;
 
 public class Gui extends JFrame {
 
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextArea resultArea;
+	private int valid = 0;
+	private int unvalid = 0;
 
 
 	public Gui() {
-		setTitle("Lightshot link scraper");
+		setTitle("Lightshot Dumper");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 317, 337);
+		setBounds(100, 100, 279, 399);
 		setResizable(false);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
@@ -39,21 +44,42 @@ public class Gui extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		resultArea = new JTextArea();
-		resultArea.setFont(new Font("Monospaced", Font.PLAIN, 12));
-
-		JScrollPane scroll = new JScrollPane(resultArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
-				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
-		scroll.setBounds(10, 11, 290, 239);
+		DefaultListModel<String> dlm = new DefaultListModel<String>();
+	    JList<String> list = new JList<>(dlm);
+	    list.addMouseListener(new MouseAdapter() {
+	        public void mouseClicked(MouseEvent e) {
+	            if (e.getClickCount() == 2) {
+	            	String value = (String)list.getModel().getElementAt(list.locationToIndex(e.getPoint()));
+	                Utils.openWebpage(value);
+	            }
+	        }
+	    });
+	    list.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		list.setBounds(322, 11, 258, 239);
+		
+		JScrollPane scroll = new JScrollPane(list, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		scroll.setBounds(10, 14, 254, 251);
+		
 		contentPane.add(scroll);
 		
+		JLabel validLabel = new JLabel("Valid: 0");
+		validLabel.setForeground(new Color(0, 128, 0));
+		validLabel.setBounds(10, 269, 254, 14);
+		contentPane.add(validLabel);
+		
+		JLabel unvalidLabel = new JLabel("Not valid: 0");
+		unvalidLabel.setForeground(new Color(255, 0, 0));
+		unvalidLabel.setBounds(11, 287, 253, 14);
+		contentPane.add(unvalidLabel);
+		
 		JSpinner countBox = new JSpinner();
-		countBox.setBounds(118, 259, 73, 20);
+		countBox.setBounds(52, 312, 73, 20);
 		contentPane.add(countBox);
 		
 		JButton scrapButton = new JButton("SCRAP LINKS");
 		scrapButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Starting...");
 				new Thread() {
 					@Override
 					public void run() {
@@ -61,7 +87,15 @@ public class Gui extends JFrame {
 						for (int i = 0; i < count; i++) {
 							try {
 								String link = Utils.checkLink(Utils.generateRandomLink(6));
-								resultArea.append(link + "\n");
+									if(link.endsWith("| VALID IMAGE!")) {
+										dlm.addElement(link.replace("| VALID IMAGE!", "") + "\n");
+										valid++;
+										
+									} else if(link.endsWith("| NOT VALID!")) {
+										unvalid++;
+									}
+									validLabel.setText("Valid: " + valid);
+									unvalidLabel.setText("Not valid: " + unvalid);
 							} catch (IOException e) {
 								e.printStackTrace();
 							}
@@ -72,18 +106,22 @@ public class Gui extends JFrame {
 			}
 		});
 		scrapButton.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		scrapButton.setBounds(10, 280, 291, 23);
+		scrapButton.setBounds(10, 343, 254, 23);
 		contentPane.add(scrapButton);
 		
 		JLabel lblNewLabel = new JLabel("by Roman");
 		lblNewLabel.setFont(new Font("Sylfaen", Font.PLAIN, 10));
-		lblNewLabel.setBounds(201, 264, 46, 14);
+		lblNewLabel.setBounds(217, 327, 46, 14);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("Count:");
-		lblNewLabel_1.setBounds(76, 261, 46, 14);
+		lblNewLabel_1.setBounds(10, 314, 46, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		countBox.setValue(5);
+		countBox.setValue(25);
+		
+		JLabel lblValidImages = new JLabel("Valid images:");
+		lblValidImages.setBounds(10, 0, 126, 14);
+		contentPane.add(lblValidImages);
 	}
 }
--- Lightshot dumper/src/me/roman/lightshotdumper/utils/Utils.java
+++ Lightshot dumper/src/me/roman/lightshotdumper/utils/Utils.java
@@ -1,7 +1,11 @@
 package me.roman.lightshotdumper.utils;
 
+import java.awt.Desktop;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.jsoup.Jsoup;
@@ -10,28 +14,42 @@ import org.jsoup.nodes.Element;
 
 public class Utils {
 
+	private static List<String> links = new ArrayList<String>();
+
 	public static String generateRandomLink(int len) {
 		char[] letters = "abcdefghijklmnopqrstuvwxyz1234567890".toCharArray();
 		String s = "";
 		for (int i = 0; i < len; i++) {
 			s += letters[new Random().nextInt(letters.length)];
+			if (links.contains(s))
+				s += letters[new Random().nextInt(letters.length)];
+
 		}
+		links.add(s);
 		return "https://prnt.sc/" + s;
 	}
 
 	public static String checkLink(String URL) throws MalformedURLException, IOException {
 		Document doc = Jsoup.connect(URL).get();
 		Element link = doc.getElementById("screenshot-image");
-		
+
 		String status = null;
 		if (link.attr("src").contains("0_173a7b_211be8ff.png")) {
-			System.out.println("NOT VALID!");
+			System.out.println(URL + " > NOT VALID!");
 			status = "NOT VALID!";
 		} else {
-			System.out.println("VALID IMAGE!");
+			System.out.println(URL + " > VALID IMAGE!");
 			status = "VALID IMAGE!";
 		}
 		return URL + " | " + status;
+	}
+
+	public static void openWebpage(String urlString) {
+		try {
+			Desktop.getDesktop().browse(new URL(urlString).toURI());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
